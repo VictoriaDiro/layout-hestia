91ecca24887b7e6ebc234ed2d505c6fe8fcff864
Victoria <victoriariadiro01@gmail.com>
2019-12-30 20:40:17 +0100

    Add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </header>
+    )
+  }
+}
+
+export default Header;
